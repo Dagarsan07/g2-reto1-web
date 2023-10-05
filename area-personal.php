6f476c26ba0816071aa9c001acfadb889527f532
H015110\ik_2dw3a <garciasanchezdavid097@gmail.com>
2023-10-05 13:02:02 +0200

    Estructura de mis publicaciones hecha. Falta estructurar parte de los datos del usuario y hacer la página responsiva

---
+++ area-personal.php
@@ -0,0 +1,73 @@
+<html>
+
+<head>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+    <title>Mi Perfil</title>
+</head>
+
+<body>
+    <div class="container mx-auto col-lg-10 col-md-11 col-sm-12">
+        <div class="row">
+            <p class="bg-danger col-lg-1">png</p>
+            <div class="col-lg-4 bg-success">
+                <p>Nombre: David García</p>
+                <p>Email: david@email.com</p>
+            </div>
+            <p class="col-lg-3 bg-info">Administrador</p>
+        </div>
+        <div class="w-75 mx-auto">
+            <h1>MIS PUBLICACIONES</h1>
+            <div class="card mt-4">
+                <div class="card-body">
+                    <div class="card-title d-flex justify-content-between">
+                        <h2>Titulo</h2>
+                        <p>2020/01/01 10:00 - David Garcia Sanchez</p>
+                    </div>
+                    <p>
+                        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Doloribus,
+                        rem accusamus ut tempore totam quibusdam atque. Eveniet, veniam mollitia
+                        laboriosam velit eius facilis numquam blanditiis aliquam vitae in nihil amet!
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis est quidem
+                        eveniet similique corporis deleniti amet deserunt fugit? Blanditiis deserunt
+                        recusandae magnam dolor quidem vero quas, rerum eos est minus.
+                    </p>
+                </div>
+            </div>
+            <div class="card mt-4">
+                <div class="card-body">
+                    <div class="card-title d-flex justify-content-between">
+                        <h2>Titulo</h2>
+                        <p>2020/01/01 10:00 - David Garcia Sanchez</p>
+                    </div>
+                    <p>
+                        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Doloribus,
+                        rem accusamus ut tempore totam quibusdam atque. Eveniet, veniam mollitia
+                        laboriosam velit eius facilis numquam blanditiis aliquam vitae in nihil amet!
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis est quidem
+                        eveniet similique corporis deleniti amet deserunt fugit? Blanditiis deserunt
+                        recusandae magnam dolor quidem vero quas, rerum eos est minus.
+                    </p>
+                </div>
+            </div>
+            <div class="card mt-4">
+                <div class="card-body">
+                    <div class="card-title d-flex justify-content-between">
+                        <h2>Titulo</h2>
+                        <p>2020/01/01 10:00 - David Garcia Sanchez</p>
+                    </div>
+                    <p>
+                        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Doloribus,
+                        rem accusamus ut tempore totam quibusdam atque. Eveniet, veniam mollitia
+                        laboriosam velit eius facilis numquam blanditiis aliquam vitae in nihil amet!
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis est quidem
+                        eveniet similique corporis deleniti amet deserunt fugit? Blanditiis deserunt
+                        recusandae magnam dolor quidem vero quas, rerum eos est minus.
+                    </p>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+
+</html>
